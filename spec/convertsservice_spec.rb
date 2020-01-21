3ea04f1e5627dff744bcf36f78c66508e7bb097e
Pavel Lobashov <shockwavenn@gmail.com>
2020-01-21 10:56:36 +0300

    Fix RuboCop `RSpec/MultipleExpectations` cop

--- spec/convertsservice_spec.rb
+++ spec/convertsservice_spec.rb
@@ -8,21 +8,21 @@ describe 'ConvertService' do
       .new('http://localhost')
   end
 
-  it 'Convert test odt file to pdf' do
-    result = converter.perform_convert(url: 'https://s3.us-west-2.amazonaws.com/nct-data-share/odt/About-Svet.odt')
-    expect(result[:url]).not_to be_nil
-    expect(result[:url]).to match(URI::DEFAULT_PARSER.make_regexp)
+  describe 'Convert test odt file to pdf' do
+    let(:target) { converter.perform_convert(url: ODT_FILE) }
+
+    it_behaves_like 'Correct Request'
   end
 
-  it 'Convert test ods file to pdf' do
-    result = converter.perform_convert(url: 'https://s3.us-west-2.amazonaws.com/nct-data-share/ods/dun6a2.ods')
-    expect(result[:url]).not_to be_nil
-    expect(result[:url]).to match(URI::DEFAULT_PARSER.make_regexp)
+  describe 'Convert test ods file to pdf' do
+    let(:target) { converter.perform_convert(url: ODS_FILE) }
+
+    it_behaves_like 'Correct Request'
   end
 
-  it 'Convert test odp file to pdf' do
-    result = converter.perform_convert(url: 'https://s3.us-west-2.amazonaws.com/nct-data-share/odp/123.odp')
-    expect(result[:url]).not_to be_nil
-    expect(result[:url]).to match(URI::DEFAULT_PARSER.make_regexp)
+  describe 'Convert test odp file to pdf' do
+    let(:target) { converter.perform_convert(url: ODP_FILE) }
+
+    it_behaves_like 'Correct Request'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,17 @@ if ENV['CI'] == 'true'
 end
 
 require 'onlyoffice_documentserver_conversion_helper'
+
+shared_examples 'Correct Request' do
+  it 'Result url is not emty' do
+    expect(target[:url]).not_to be_nil
+  end
+
+  it 'Result url is an url' do
+    expect(target[:url]).to match(URI::DEFAULT_PARSER.make_regexp)
+  end
+end
+
+ODT_FILE = 'https://s3.us-west-2.amazonaws.com/nct-data-share/odt/About-Svet.odt'
+ODS_FILE = 'https://s3.us-west-2.amazonaws.com/nct-data-share/ods/dun6a2.ods'
+ODP_FILE = 'https://s3.us-west-2.amazonaws.com/nct-data-share/odp/123.odp'
