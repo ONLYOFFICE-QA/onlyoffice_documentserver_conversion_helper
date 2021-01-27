51e1b23cc3f70eda025982aca7b7cfb644b6108b
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-01-27 15:40:58 +0300

    Release v0.2.0 (#18)

--- lib/onlyoffice_documentserver_conversion_helper/version.rb
+++ lib/onlyoffice_documentserver_conversion_helper/version.rb
@@ -4,6 +4,6 @@ module OnlyofficeDocumentserverConversionHelper
   # This module holds the OnlyofficeDocumentserverConversionHelper gem version
   module Version
     # @return [String] version of gem
-    STRING = '0.1.1'
+    STRING = '0.2.0'
   end
 end
