ae971aefb0fe85a190c3342b3346219ad0df55da
Sorokin_Maksim <askonev007@gmail.com>
2021-12-16 16:58:32 +0300

    Rename env into rspec

--- spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
+++ spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 describe XmlResponceParser do
-  let(:url) do
+  let(:parser) do
     described_class.new(File.read('assets/responce/local.xml'), 'docx')
   end
 
   it 'eq result_url for template' do
-    expect(url.result_url).to eq('http://localhost/filename=output.docx')
+    expect(parser.result_url).to eq('http://localhost/filename=output.docx')
   end
 end
