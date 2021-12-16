6ab26e58034152665fe8f67d7ad1776e3545c20b
Sorokin_Maksim <askonev007@gmail.com>
2021-12-16 18:51:59 +0300

    Exclude dublicat test for described class

--- spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
+++ spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
@@ -7,15 +7,12 @@ describe XmlResponceParser do
     described_class.new(File.read('assets/responce/formatte_responce_example.xml'), 'docx')
   end
 
-  let(:parser_for_unformatted_xml) do
-    described_class.new(File.read('assets/responce/unformatte_response_example.xml'), 'docx')
-  end
-
   it 'eq result_url for template' do
     expect(parser.result_url).to eq('http://localhost/filename=output.docx')
   end
 
   it 'eq result_url for unformatted template' do
-    expect(parser_for_unformatted_xml.result_url).to eq('http://localhost/filename=output.docx')
+    parser = described_class.new(File.read('assets/responce/unformatte_response_example.xml'), 'docx')
+    expect(parser.result_url).to eq('http://localhost/filename=output.docx')
   end
 end
