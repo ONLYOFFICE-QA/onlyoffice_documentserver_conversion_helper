dd09cf65dfce43311b437c7ccfa4084f42e478d0
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2021-12-17 10:49:03 +0300

    Add rspec for unformatted xml (#111)
    * Add rspec for unformatted xml
    
    * Exclude dublicat test for described class
    
    * Added clarification into regexp script
    
    * Add changes in change log

--- spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
+++ spec/onlyoffice_documentserver_conversion_helper/xml_responce_parser_spec.rb
@@ -10,4 +10,9 @@ describe XmlResponceParser do
   it 'eq result_url for template' do
     expect(parser.result_url).to eq('http://localhost/filename=output.docx')
   end
+
+  it 'eq result_url for unformatted template' do
+    parser = described_class.new(File.read('assets/responce/unformatte_response_example.xml'), 'docx')
+    expect(parser.result_url).to eq('http://localhost/filename=output.docx')
+  end
 end
